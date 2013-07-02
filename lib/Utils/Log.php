c024e7f79218b00a8e5fcdaa70fa4c95700a75ef
smagch <smagch@gmail.com>
2013-07-03 02:40:33 +0900

    add logger

---
+++ lib/Utils/Log.php
@@ -0,0 +1,21 @@
+<?php
+namespace Utils;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Config;
+
+class Log
+{
+    /**
+     * @return Monolog\Logger
+     */
+    public static function getLogger()
+    {
+        $config = Config::getConfig();
+        $log = new Logger('procon_u16');
+        $level = strtoupper($config->logging_level);
+        $log->pushHandler(new StreamHandler($config->logging_path, constant("Logger::$level")));
+        return $log;
+    }
+}
