05c60b54366a432f23436be5f25907222186cc94
smagch <smagch@gmail.com>
2013-07-03 01:46:32 +0900

    add mailer module

---
+++ lib/Utils/Mailer.php
@@ -0,0 +1,46 @@
+<?php
+namespace Utils;
+
+use Config;
+use PHPMailer;
+
+class Mailer
+{
+    /**
+     * @param string   email address to send
+     * @param string   email content to send
+     * @param boolean  success or fail
+     */
+    public static function send($address, $body)
+    {
+        $config = Config::getConfig();
+
+        mb_language("japanese");
+        mb_internal_encoding("UTF-8");
+
+        $mail = new PHPMailer();
+        $mail->IsHTML(false);
+        $mail->IsSMTP();
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $mail->SMTPDebug  = 0;
+        $mail->CharSet = "iso-2022-jp";
+        $mail->Encoding = "7bit";
+        $mail->Host       = "smtp.lolipop.jp";
+        $mail->Port       = 25;
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $config->mailer_username;
+        $mail->Password   = $config->mailer_password;
+        $mail->SetFrom($config->mailer_username, '');
+        $mail->AddAddress($address, '');
+        $mail->Subject = 'U16 Asahikawa Programming Contest Email Nortification';
+        $mail->Body    = mb_convert_encoding($body,"JIS","UTF-8");
+        // TODO error logging here
+        if(!$mail->Send()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
