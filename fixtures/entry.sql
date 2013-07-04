338fe6d293641be962263f487b002a704932ca16
smagch <smagch@gmail.com>
2013-07-04 14:48:15 +0900

    update entry schema

--- fixtures/entry.sql
+++ fixtures/entry.sql
@@ -6,19 +6,22 @@
 -- DROP TABLE IF EXISTS `entry`;
 --
 CREATE TABLE `entry` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `school_name` varchar(100) NOT NULL,
-  `grade` tinyint unsigned NOT NULL,
-  `category_id` tinyint unsigned NOT NULL,
-  `lecture_id` tinyint unsigned NOT NULL,
-  `lecture_pref_day_one` tinyint unsigned NOT NULL,
-  `lecture_pref_day_two` tinyint unsigned NOT NULL,
-  `lecture_pref_day_three` tinyint unsigned NOT NULL,
-  `lecture_pref_day_four` tinyint unsigned NOT NULL,
+  `grade` tinyint(1) unsigned NOT NULL,
+  `category` enum('kyogi','sakuhin','jugyo') NOT NULL,
+  `category_kyogi_q_macro` enum('yes','no') DEFAULT NULL,
+  `category_kyogi_q_exp` enum('yes','no') DEFAULT NULL,
+  `lecture_pref_day_one` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_two` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_three` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_four` tinyint(1) unsigned NOT NULL,
   `comment` text NOT NULL,
-  `lecture_comment` text NOT NULL,
-  `category_comment` text NOT NULL,
+  `comment_kyogi` text NOT NULL,
+  `comment_sakuhin` text NOT NULL,
+  `comment_jugyo` text NOT NULL,
+  `comment_lecture` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
