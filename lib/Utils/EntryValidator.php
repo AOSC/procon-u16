7c4d1d41afa5e8efdb0dab75a503f426d65eba83
smagch <smagch@gmail.com>
2013-07-05 14:16:16 +0900

    update entry validator

--- lib/Utils/EntryValidator.php
+++ lib/Utils/EntryValidator.php
@@ -2,22 +2,64 @@
 namespace Utils;
 use Respect\Validation\Validator as v;
 
+/*
+CREATE TABLE `entry` (
+  `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `school_name` varchar(100) NOT NULL,
+  `grade` tinyint(1) unsigned NOT NULL,
+  `category` set('kyogi','sakuhin','jugyo') NOT NULL,
+  `q_kyogi_macro` enum('yes','no', '') DEFAULT '',
+  `q_kyogi_exp` enum('yes','no', '') DEFAULT '',
+  `lecture_pref_day_one` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_two` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_three` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_four` tinyint(1) unsigned NOT NULL,
+  `comment` text NOT NULL,
+  `comment_kyogi` text NOT NULL,
+  `comment_sakuhin` text NOT NULL,
+  `comment_jugyo` text NOT NULL,
+  `comment_lecture` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+*/
+
 class EntryValidator
 {
     /**
-     * validate model
+     * validate entity model
+     *
+     * 本当は、Validationは１度に行い、エラーメッセージは自動的に吐いて欲しいのですが、
+     * Respect\Validationのv0.4.4の現状ではもんだがまだ解決されていません
+     * 詳しくは、以下のリンクのチケットをみて下さい
+     * https://github.com/Respect/Validation/issues/86
+     *
+     *   e.g. v::arr()->key('username', v::string())
+     *                ->key('email',    v::email())
+     *                ->assert($model);
+     *
+     * @param  array   entity model to validate
+     * @return array   list of error messages
      */
     public static function validate(array $model)
     {
         $errors = array();
+        mb_language("japanese");
+        mb_internal_encoding("UTF-8");
 
         // at least 1 letter
         if (! v::arr()->key('name', v::string()->length(1, null))->validate($model)) {
             $errors[] = '名前は必須です';
+        } elseif (mb_strlen($model['name']) > 100) {
+            $errors[] = '名前は100文字以下でお願いします';
         }
 
         if (! v::arr()->key('school_name', v::string()->length(1, null))->validate($model)) {
             $errors[] = '学校名は必須です';
+        } elseif (mb_strlen($model['school_name']) > 100) {
+            $errors[] = '名前は100文字以下でお願いします';
         }
 
         $one_to_three = v::int()->between(1, 3, true);
@@ -26,12 +68,16 @@ class EntryValidator
             $errors[] = '学年は、1、2、3のどれかでなければいけません';
         }
 
-        if (! v::arr()->key('category_id', $one_to_three)->validate($model)) {
-            $errors[] = '参加部門は必須です';
-        }
-
-        if (! v::arr()->key('lecture_id', $one_to_three)->validate($model)) {
-            $errors[] = '参加部門は必須です';
+        if (! v::arr()->key('category', v::arr())->validate($model)) {
+            $errors[] = '参加部門が不正な値です';
+            $errors[] = '参加部門は、競技・作品・授業のいずれか１つ以上選択する必要があります';
+        } elseif (empty($model['category'])) {
+            $errors[] = '参加部門は、競技・作品・授業のいずれか１つ以上選択する必要があります';
+        } else {
+            $diff = array_diff($model['category'], array('kyogi', 'sakuhin', 'jugyo'));
+            if (! empty($diff)) {
+                $errors[] = implode(', ', $diff) . 'は不正なIDです';
+            }
         }
 
         if (! v::arr()->key('lecture_pref_day_one', $one_to_three)->validate($model)) {
@@ -57,16 +103,28 @@ class EntryValidator
             $errors[] = 'コメントが多すぎです';
         }
 
-        if (! v::arr()->key('lecture_comment', v::string())->validate($model)) {
+        if (! v::arr()->key('comment_lecture', v::string())->validate($model)) {
             $errors[] = '講習会についての希望が文字ではありません';
-        } elseif (mb_strlen($model['lecture_comment'] > 64000)) {
+        } elseif (mb_strlen($model['comment_lecture'] > 64000)) {
             $errors[] = '講習会についての希望コメントが多すぎです';
         }
 
-        if (! v::arr()->key('category_comment', v::string())->validate($model)) {
-            $errors[] = '質問が文字ではありません';
-        } elseif (mb_strlen($model['lecture_comment']) > 64000) {
-            $errors[] = '質問のコメントが多すぎです';
+        if (! v::arr()->key('comment_kyogi', v::string())->validate($model)) {
+            $errors[] = '競技の質問が文字ではありません';
+        } elseif (mb_strlen($model['comment_kyogi']) > 64000) {
+            $errors[] = '競技の質問のコメントが多すぎです';
+        }
+
+        if (! v::arr()->key('comment_sakuhin', v::string())->validate($model)) {
+            $errors[] = '作品についてのコメントが文字ではありません';
+        } elseif (mb_strlen($model['comment_sakuhin']) > 64000) {
+            $errors[] = '作品についてののコメントが多すぎです';
+        }
+
+        if (! v::arr()->key('comment_jugyo', v::string())->validate($model)) {
+            $errors[] = 'マイコンカーについてのコメントが文字ではありません';
+        } elseif (mb_strlen($model['comment_jugyo']) > 64000) {
+            $errors[] = 'マイコンカーについてのコメントが多すぎです';
         }
 
         return $errors;
--- tests/Utils/EntryValidatorTest.php
+++ tests/Utils/EntryValidatorTest.php
@@ -4,20 +4,23 @@ use Utils\EntryValidator as V;
 /* entry form schema
 
 CREATE TABLE `entry` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `school_name` varchar(100) NOT NULL,
-  `grade` tinyint unsigned NOT NULL,
-  `category_id` tinyint unsigned NOT NULL,
-  `lecture_id` tinyint unsigned NOT NULL,
-  `lecture_pref_day_one` tinyint unsigned NOT NULL,
-  `lecture_pref_day_two` tinyint unsigned NOT NULL,
-  `lecture_pref_day_three` tinyint unsigned NOT NULL,
-  `lecture_pref_day_four` tinyint unsigned NOT NULL,
+  `grade` tinyint(1) unsigned NOT NULL,
+  `category` set('kyogi','sakuhin','jugyo') NOT NULL,
+  `q_kyogi_macro` enum('yes','no', '') DEFAULT '',
+  `q_kyogi_exp` enum('yes','no', '') DEFAULT '',
+  `lecture_pref_day_one` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_two` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_three` tinyint(1) unsigned NOT NULL,
+  `lecture_pref_day_four` tinyint(1) unsigned NOT NULL,
   `comment` text NOT NULL,
-  `lecture_comment` text NOT NULL,
-  `category_comment` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_kyogi` text NOT NULL,
+  `comment_sakuhin` text NOT NULL,
+  `comment_jugyo` text NOT NULL,
+  `comment_lecture` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
@@ -27,23 +30,23 @@ class EntryValidatorTest extends PHPUnit_Framework_TestCase
     public function testValidateWithValidData()
     {
         $valid_data = array(
-            'name' => 'foo bar',
+            'name' => '島口知也',
             'school_name' => '旭川市東陽中学校',
             'grade' => 1,
-            'category_id' => 2,
-            'lecture_id' => 3,
+            'category' => array('kyogi', 'sakuhin'),
             'lecture_pref_day_one' => 1,
             'lecture_pref_day_two' => 2,
             'lecture_pref_day_three' => 2,
             'lecture_pref_day_four' => 3,
             'comment' => '',
-            'lecture_comment' => '',
-            'category_comment' => '',
+            'comment_lecture' => '',
+            'comment_kyogi' => '',
+            'comment_sakuhin' => '',
+            'comment_jugyo' => ''
         );
 
         $result = V::validate($valid_data);
         $this->assertEmpty($result);
-
     }
 
     public function testValidateWithInvalidData()
@@ -52,18 +55,19 @@ class EntryValidatorTest extends PHPUnit_Framework_TestCase
             'name' => '',
             'school_name' => '',
             'grade' => '',
-            'category_id' => 10,
-            'lecture_id' => 4,
+            'category' => array('invalid'),
             'lecture_pref_day_one' => 4,
             'lecture_pref_day_two' => 5,
             'lecture_pref_day_three' => 6,
             'lecture_pref_day_four' => 7,
             'comment' => null,
-            'lecture_comment' => null,
-            'category_comment' => null,
+            'comment_lecture' => null,
+            'comment_kyogi' => null,
+            'comment_sakuhin' => null,
+            'comment_jugyo' => null,
         );
 
         $result = V::validate($invalid_data);
-        $this->assertCount(12, $result);
+        $this->assertCount(13, $result);
     }
 }
